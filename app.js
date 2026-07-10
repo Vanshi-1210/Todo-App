950e2ec607b79d01a07967a763ffcb1188833fc0
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 11:39:37 +0530

    feat: implement basic todo logic for add, list, and quit actions

---
+++ app.js
@@ -0,0 +1,25 @@
+let todo=[];
+ 
+let req=prompt("Please enter your request");
+
+while(true){
+    if(req=="quit"){
+        console.log("quitting app");
+        break;
+    }
+
+    if(req=="list"){
+        console.log("---------------");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("---------------");
+    }else if(req=="add"){
+        let task=prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added")
+    }
+    
+    req=prompt("Please enter your request");
+
+}
